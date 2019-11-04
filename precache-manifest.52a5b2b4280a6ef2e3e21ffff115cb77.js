2739ace3eeaee3e91f16b3c782d7b4becbd8fe9e
DESKTOP-GHPUBQT\gogos <gogosergey@gmail.com>
2019-11-04 18:07:28 +0200

    Updates

---
+++ precache-manifest.52a5b2b4280a6ef2e3e21ffff115cb77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72342d4b08362d9fa3c9921daa16a3b2",
+    "url": "/image-color-sort/index.html/index.html"
+  },
+  {
+    "revision": "a1732a29196ba2d88be3",
+    "url": "/image-color-sort/index.html/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "48555b13739765c29a21",
+    "url": "/image-color-sort/index.html/static/css/main.25d650f2.chunk.css"
+  },
+  {
+    "revision": "a1732a29196ba2d88be3",
+    "url": "/image-color-sort/index.html/static/js/2.f36efb85.chunk.js"
+  },
+  {
+    "revision": "48555b13739765c29a21",
+    "url": "/image-color-sort/index.html/static/js/main.ffa89bb0.chunk.js"
+  },
+  {
+    "revision": "d6dc3cdbcff661944ec3",
+    "url": "/image-color-sort/index.html/static/js/runtime-main.068e15d3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/image-color-sort/index.html/static/media/logo.25bf045c.svg"
+  }
+]);
